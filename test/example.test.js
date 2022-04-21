1bda30f51de421e3db859f8387354fb6d26f41ca
Ercan Er <102952533+ercan-er@users.noreply.github.com>
2022-04-21 13:39:55 +0300

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -7,4 +7,8 @@ describe('Test Suite Which Contains Success', () => {
        it('Test Will Be Secon Pass', () => {
         expect(true).toEqual(true);
     });
+    
+     it('Test Will Be Secon Pass', () => {
+        expect(false).toEqual(true);
+    });
 });
